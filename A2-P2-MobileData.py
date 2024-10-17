d1b5f346c8091bf6c3269bc4817e3343f66b5150
Your Name <w0402993@nscc.ca>
2024-10-16 22:04:31 -0300

    Program finished

--- A2-P2-MobileData.py
+++ A2-P2-MobileData.py
@@ -5,18 +5,30 @@
 # Note, it is not a cumulative charge; your program will figure out which single range the usage falls into, 
 # then calculate the cost based on that range’s cost. 
 
-#Student #:     
-#Student Name:  
+#Student #:     w0402993
+#Student Name:  Andrew Beaver
 
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
+    #Display welcome message
+    print("Erewhon Mobile Data Plans")
 
+    #Prompt user for their data usage
+    usage = float(input("\nEnter data usage (Mb): "))
 
+    #Establish statements to get the rate for each total data usage category
+    if usage <= 200:
+        usageRate = 20
+    elif usage > 200 and usage <= 500:
+        usageRate = usage * 0.105
+    elif usage > 500 and usage <= 1024:
+        usageRate = usage * 0.11
+    else:
+        usageRate = 118
 
-
-
-
+    #Display the data fee to the user
+    print("\nTotal charge is ${0:.2f}".format(usageRate)) 
 
     # YOUR CODE ENDS HERE
 
